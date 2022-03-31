0cb986cea3398858cecf1c27a09cded6dec6a190
Jewell <101367908+jewellweb@users.noreply.github.com>
2022-04-01 00:20:12 +0300

    Update server.lua

--- jwl-weed/server.lua
+++ jwl-weed/server.lua
@@ -9,7 +9,8 @@
          # # # #        # # #           # # #      # # # # # #        
 --]]
 
-local QBCore = exports['qb-core']:GetCoreObject()
+QBCore = nil
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
 -- KENEVIR SATMA
 
