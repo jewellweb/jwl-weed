0db41f7fda42c70e64feb30c003e388fec726844
Jewell <101367908+jewellweb@users.noreply.github.com>
2022-04-01 00:06:07 +0300

    Update client.lua

--- jwl-weed/client.lua
+++ jwl-weed/client.lua
@@ -9,13 +9,7 @@
          # # # #        # # #           # # #      # # # # # #        
 --]]
 
-QBCore = nil
-Citizen.CreateThread(function()
-    while QBCore == nil do
-        TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-        Citizen.Wait(200)
-    end
-end)
+local QBCore = exports['qb-core']:GetCoreObject()
 
 -- KENEVIR SATMA
 
